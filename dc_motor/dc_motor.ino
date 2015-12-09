1e53b33310365203e2d014aee4a0cce18bf69698
paragekhande@gmail.com <paragekhande@parags-MacBook-Pro.local>
2015-12-09 18:08:43 +0530

    1st Update

---
+++ dc_motor/dc_motor.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(9,OUTPUT);
+
+pinMode(10,OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.println("Working");
+  // put your main code here, to run repeatedly:
+digitalWrite(9,HIGH);
+digitalWrite(10,LOW);
+delay(5000);
+digitalWrite(9,LOW);
+digitalWrite(10,LOW);
+delay(3000);
+digitalWrite(9,LOW);
+digitalWrite(10,HIGH);
+delay(5000);
+digitalWrite(9,LOW);
+digitalWrite(10,LOW);
+delay(3000);
+}
